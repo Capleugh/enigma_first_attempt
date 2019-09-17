fc17f9d64aafad0741aab8c507dd165c76d4bbf2
Carleigh Crockett <capleugh@gmail.com>
2019-09-17 17:27:53 -0600

    Adds method for splitting last four digits

--- lib/offset.rb
+++ lib/offset.rb
@@ -9,6 +9,18 @@ class Offset
 
   def square(date)
     date.to_i * date.to_i
+    # require pry'; binding.pry
+  end
+
+  def last_four(square)
+    square.to_s[-4..-1]
     # require 'pry'; binding.pry
   end
+
+  def split(last_four_digits)
+    last_four_digits.chars
+  end
+
+  # def generate_offset(last_four_digits)
+  # end
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -19,4 +19,25 @@ class OffsetTest < Minitest::Test
   def test_date_can_be_squared
     assert_equal 29_213_304_561, @offset.square("170919")
   end
+
+  def test_isolate_last_four_digits_of_squared_date
+    assert_equal "4561", @offset.last_four(29_213_304_561)
+  end
+
+  def test_last_four_digits_can_be_split
+    expected = ["4", "5", "6", "1"]
+
+    assert_equal expected, @offset.split("4561")
+  end
+
+  # def test_offset_can_be_generated_by_random_number
+  #   expected = {
+  #     A: "4",
+  #     B: "5",
+  #     C: "6",
+  #     D: "1",
+  #   }
+  #
+  #   assert_equal expected, @offset.generate_offset("4561")
+  end
 end
