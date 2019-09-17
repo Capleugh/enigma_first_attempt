cc99e3f8e1c9120ee6d3f3b6a082372ada120e53
Carleigh Crockett <capleugh@gmail.com>
2019-09-16 21:43:19 -0600

    Updates key class to account for actual random number

--- lib/key.rb
+++ lib/key.rb
@@ -1,29 +1,27 @@
-class Keys
+require './lib/random_number'
 
-  def initialize(number)
-    @number = number
+class Key
+
+  def initialize
+    @number = RandomNumber.new.num
   end
 
-  def number
-    @number
-  end
-
-  def split(random_number)
+  def split(random_number = @number)
     random = []
     random_number.chars.each_cons(2) do |number|
-      random << number[0] + number[1] 
+      random << number[0] + number[1]
       # require 'pry'; binding.pry
     end
     random
   end
 
-  def generate_key(random_number)
-    # require 'pry'; binding.pry
-    keys_hash = {
+  def generate_key(random_number = @number)
+    key_hash = {
+      # come back and check out why this is yelling at you
       A: split(random_number)[0],
       B: split(random_number)[1],
       C: split(random_number)[2],
-      D: split(random_number)[3]
+      D: split(random_number)[3],
     }
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,27 +1,29 @@
 require './test_helper'
 require './lib/random_number'
-require './lib/keys'
+require './lib/key'
 
 
 class KeysTest < Minitest::Test
   def setup
     @random = RandomNumber.new
     @r2 = RandomNumber.new.num
-    @keys =Keys.new("02715")
+    @key =Key.new
   end
 
   def test_it_exists
-    assert_instance_of Keys, @keys
+    assert_instance_of Key, @key
   end
 
-  def test_initialize
-    assert_equal "02715", @keys.number
-  end
+  # def test_initialize
+  #   # @key.instance_variable_set(:@number, "02715")
+  #   assert_equal "02715", @key.number
+  #   # mock this number?
+  # end
 
   def test_random_number_can_be_split
     expected = ["02", "27", "71", "15"]
 
-    assert_equal expected, @keys.split("02715")
+    assert_equal expected, @key.split("02715")
   end
 
   def test_keys_can_be_generated_from_random_number
@@ -32,7 +34,7 @@ class KeysTest < Minitest::Test
       D: "15",
     }
 
-    assert_equal expected, @keys.generate_key("02715")
+    assert_equal expected, @key.generate_key("02715")
   end
 
 end
