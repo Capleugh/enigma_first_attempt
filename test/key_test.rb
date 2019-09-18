5691a9a688b221d1b8a3919aada37e9198f077d3
Carleigh Crockett <capleugh@gmail.com>
2019-09-17 18:35:50 -0600

    Updates tests to be more specific

--- test/key_test.rb
+++ test/key_test.rb
@@ -5,26 +5,29 @@ require './module/randomable'
 
 class KeysTest < Minitest::Test
   def setup
-    @key = Key.new
-    @key = Key.new("02715")
+    @key_1 = Key.new
+    @key_2 = Key.new("02715")
   end
 
   def test_it_exists
-    assert_instance_of Key, @key
+    assert_instance_of Key, @key_1
+    assert_instance_of Key, @key_2
   end
 
-  def test_random_number_length
-    assert_equal "02715".length, @key.generate.length
+  def test_number_length
+    assert_equal "02715".length, @key_2.generate.length
+    # require 'pry'; binding.pry
   end
 
   def test_numbers_data_type
-    assert_instance_of String, @key.generate
+    assert_instance_of String, @key_1.generate
+    assert_instance_of String, @key_2.generate
   end
 
   def test_random_number_can_be_split
     expected = ["02", "27", "71", "15"]
 
-    assert_equal expected, @key.split("02715")
+    assert_equal expected, @key_2.split
   end
 
   def test_keys_can_be_generated_from_random_number
@@ -35,7 +38,7 @@ class KeysTest < Minitest::Test
       D: "15",
     }
 
-    assert_equal expected, @key.generate_key("02715")
+    assert_equal expected, @key_2.generate_key("02715")
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -4,40 +4,45 @@ require './module/dateable'
 
 class OffsetTest < Minitest::Test
   def setup
-    @offset = Offset.new
-    @offset = Offset.new("091719")
+    @offset_1 = Offset.new
+    @offset_2 = Offset.new("091719")
   end
 
   def test_it_exists
-    assert_instance_of Offset, @offset
+    assert_instance_of Offset, @offset_1
   end
 
   def test_date_can_be_generated
-    assert_equal "170919".length, @offset.generate.length
+    assert_equal "170919".length, @offset_2.generate.length
+  end
+
+  def test_generated_date_data_type
+    assert_instance_of String, @offset_1.generate
+    assert_instance_of String, @offset_2.generate
   end
 
   def test_date_can_be_squared
-    assert_equal 29_213_304_561, @offset.square("170919")
+    assert_equal 29_213_304_561, @offset_2.square("170919")
   end
 
   def test_isolate_last_four_digits_of_squared_date
-    assert_equal "4561", @offset.last_four(29_213_304_561)
+    assert_equal "4561", @offset_2.last_four(29_213_304_561)
   end
 
   def test_last_four_digits_can_be_split
     expected = ["4", "5", "6", "1"]
 
-    assert_equal expected, @offset.split("4561")
+    assert_equal expected, @offset_2.split("4561")
   end
 
-  # def test_offset_can_be_generated_by_random_number
-  #   expected = {
-  #     A: "4",
-  #     B: "5",
-  #     C: "6",
-  #     D: "1",
-  #   }
-  #
-  #   assert_equal expected, @offset.generate_offset("4561")
+  def test_offset_can_be_generated_by_random_number
+    expected = {
+      A: "4",
+      B: "5",
+      C: "6",
+      D: "1",
+    }
+
+    assert_equal expected, @offset_2.generate_offset("4561")
   end
 end
