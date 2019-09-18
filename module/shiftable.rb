8507f53b9bd3525e9f0cbbca443607e41a678186
Carleigh Crockett <capleugh@gmail.com>
2019-09-18 06:35:26 -0600

    Updates modules

---
+++ module/shiftable.rb
@@ -0,0 +1,13 @@
+# require './test_helper'
+# require './lib/key'
+# require './lib/offset'
+# require './lib/encryptor'
+
+# module Shiftable
+  # def shift
+  #   @key.values.each do |num|
+  #     require 'pry'; binding.pry
+  #
+  #   end
+  # end
+# end
