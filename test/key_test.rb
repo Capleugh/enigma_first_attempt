ee8a6e06ce5b398724e8e6a457f2f544271a0af3
Carleigh Crockett <capleugh@gmail.com>
2019-09-18 06:32:59 -0600

    Adds convert method and test

--- test/key_test.rb
+++ test/key_test.rb
@@ -14,31 +14,33 @@ class KeysTest < Minitest::Test
     assert_instance_of Key, @key_2
   end
 
-  def test_number_length
+  def test_initialize
     assert_equal "02715".length, @key_2.generate.length
-    # require 'pry'; binding.pry
-  end
-
-  def test_numbers_data_type
     assert_instance_of String, @key_1.generate
     assert_instance_of String, @key_2.generate
   end
 
-  def test_random_number_can_be_split
+  def test_random_number_can_be_split_to_get_key
     expected = ["02", "27", "71", "15"]
 
-    assert_equal expected, @key_2.split
+    assert_equal expected, @key_2.generate_key
   end
 
-  def test_keys_can_be_generated_from_random_number
-    expected = {
-      A: "02",
-      B: "27",
-      C: "71",
-      D: "15",
-    }
+  def test_string_to_i
+    expected = [2, 27, 71, 15]
 
-    assert_equal expected, @key_2.generate_key("02715")
+    assert_equal expected, @key_2.convert
   end
+
+  # def test_keys_can_be_generated_from_random_number
+  #   expected = {
+  #     A: "02",
+  #     B: "27",
+  #     C: "71",
+  #     D: "15",
+  #   }
+  #
+  #   assert_equal expected, @key_2.generate_key("02715")
+  # end
 
 end
