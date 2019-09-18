3f7acecff76772afd4c555a0d595ef113ecfba29
Carleigh Crockett <capleugh@gmail.com>
2019-09-18 07:48:01 -0600

    Updates generate_offset method and test

--- lib/offset.rb
+++ lib/offset.rb
@@ -10,31 +10,16 @@ class Offset
 
   def square(date)
     date.to_i * date.to_i
-    # require 'pry'; binding.pry
   end
 
   def last_four(square)
     square.to_s[-4..-1]
-    # require 'pry'; binding.pry
   end
 
   def generate_offset(last_four)
     @offset = last_four.chars
-    # require 'pry'; binding.pry
-  end
-
-  def convert
-    @offset.map do |number|
-      number.to_i
+    @offset.map do |element|
+      element.to_i
     end
   end
-
-  # def generate_offset(last_four_digits)
-  #   @offset = {
-  #     A: split(last_four_digits)[0],
-  #     B: split(last_four_digits)[1],
-  #     C: split(last_four_digits)[2],
-  #     D: split(last_four_digits)[3],
-  #   }
-  # end
 end
